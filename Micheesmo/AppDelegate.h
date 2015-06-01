1fb81d5f54773b6f98967dd7c82b54de524516ec
petey3 <ericdpetey@gmail.com>
2015-06-01 19:15:10 -0400

    Initial Commit

---
+++ Micheesmo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Micheesmo
+//
+//  Created by Eric Peterson on 6/1/15.
+//  Copyright (c) 2015 Eric Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Micheesmo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Micheesmo
+//
+//  Created by Eric Peterson on 6/1/15.
+//  Copyright (c) 2015 Eric Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
