f6922678fe55878c39a6decfab30e61b3cd18522
petey3 <ericdpetey@gmail.com>
2015-06-02 00:57:18 -0400

    Preparing work on the View
    So when i couldn’t add a ClassPrefix, it also meant that it tied the
    assistant for the view controller to just “ViewController” so the
    assistant was looking for that instead of “CardGameViewController” so I
    axed the prefix because bleh.

--- Micheesmo/ViewController.h
+++ Micheesmo/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CardGameViewController : UIViewController
+@interface ViewController : UIViewController
 
 
 @end
---
+++ Micheesmo/ViewController.m
@@ -0,0 +1,17 @@
+//
+//  ViewController.m
+//  Micheesmo
+//
+//  Created by Eric Peterson on 6/1/15.
+//  Copyright (c) 2015 Eric Peterson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@end
