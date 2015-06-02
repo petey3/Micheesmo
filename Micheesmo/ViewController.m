6b716e03761715f807be26c8b1628c1d1b8cab50
petey3 <ericdpetey@gmail.com>
2015-06-02 01:30:36 -0400

    Card flip
    But stays the same card

--- Micheesmo/ViewController.m
+++ Micheesmo/ViewController.m
@@ -14,4 +14,25 @@
 
 @implementation ViewController
 
+- (IBAction)touchCardButton:(UIButton *)sender
+{
+    UIImage* cardImg;// = [UIImage imageNamed:@"card back"];
+    NSString* title;
+    //Assign new image and card based on the current ones
+    if([sender.currentTitle length])
+    {
+        cardImg = [UIImage imageNamed:@"card back"];
+        title = @"";
+    }
+    else
+    {
+        cardImg = [UIImage imageNamed:@"card front"];
+        title = @"A♠️";
+    }
+    
+    //Set the image and title
+    [sender setBackgroundImage:cardImg forState:UIControlStateNormal];
+    [sender setTitle:title forState:UIControlStateNormal];
+}
+
 @end
